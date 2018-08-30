020d198b79c2a02f8e037dd26423b9e7a9b1eb94
ManalLiaquat <m.manalliaquat@gmail.com>
2018-08-30 23:17:57 +0500

    Testing GET for /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,6 +5,14 @@ const { app } = require("../server");
 const { Todo } = require("../models/todo");
 // const { User } = require("../models/user");
 
+const todos = [
+  {
+    text: "First test todo"
+  },
+  {
+    text: "Second test todo"
+  }
+]; 
 beforeEach(done => {
   Todo.deleteMany({}).then(() => done());
 });
@@ -24,13 +32,13 @@ describe("POST /todos", () => {
         if (err) {
           return done(err);
         }
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
             done();
           })
-          .catch(e => console.log(e));
+          .catch(e => done(e));
       });
   });
 
@@ -43,10 +51,24 @@ describe("POST /todos", () => {
         if (err) {
           return done(err);
         }
-        Todo.find().then(todos => {
-          expect(todos.length).toBe(0);
-          done();
-        });
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(2);
+            done();
+          })
+          .catch(e => done(e));
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
