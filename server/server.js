4b7a30cc96efa0ba2402575c6bca1c6418a1e578
ManalLiaquat <m.manalliaquat@gmail.com>
2018-08-30 22:49:53 +0500

    Adding GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,11 +10,24 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
-  //   console.log(req.body);
+  console.log(
+    `\n[Request] POST => [Route] /todos\n\n${JSON.stringify(req.body,undefined,3)}`
+  );
   let newTodo = new Todo({
     text: req.body.text
   });
   newTodo.save().then(item => res.send(item), err => res.status(400).send(err));
+});
+
+app.get("/todos", (req, res) => {
+  console.log(`\n[Request] GET => [Route] /todos\n\n`);
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+      console.log(JSON.stringify(todos,undefined,2));      
+    },
+    err => res.status(400).send(err)
+  );
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
