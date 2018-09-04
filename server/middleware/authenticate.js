41514535b9c1baaaac646c8d0fcea82134740cfb
ManalLiaquat <m.manalliaquat@gmail.com>
2018-09-04 22:22:32 +0500

    Associate users with todos

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,17 +1,20 @@
-const { User } = require("../models/user");
+var { User } = require("./../models/user");
 
-let authenticate = (req, res, next) => {
-  let token = req.header("x-auth");
+var authenticate = (req, res, next) => {
+  var token = req.header("x-auth");
+
   User.findByToken(token)
     .then(user => {
       if (!user) {
         return Promise.reject();
       }
+
       req.user = user;
       req.token = token;
+      next();
     })
     .catch(e => {
-      res.status(401).send(e);
+      res.status(401).send();
     });
 };
 
