845ade5b33056c40e5fb06dfa2576f9506776baa
ManalLiaquat <m.manalliaquat@gmail.com>
2018-09-05 18:05:31 +0500

    Authenticating user for all routes of todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,5 +5,6 @@ mongoose.connect(
   process.env.MONGOLAB_URI,
   { useNewUrlParser: true }
 );
+console.log(`process.env.MONGOLAB_URI ****** ${process.env.MONGOLAB_URI} => FILE:(mongoose.js)`);
 
 module.exports = { mongoose };
